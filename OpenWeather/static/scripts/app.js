e36d7e81ae1cc2efae59c0ffef72c1285b42ef05
Solar <solarcellsky@gmail.com>
2015-11-19 10:13:11 +0800

    '..'

--- OpenWeather/static/scripts/app.js
+++ OpenWeather/static/scripts/app.js
@@ -278,10 +278,10 @@ define(function(require) {
         var cnt = 6;
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
-                var data_current = 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&lang=zh_cn&callback=?';
-                var data_hourly = 'http://api.openweathermap.org/data/2.5/forecast/hourly?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&cnt=1&units=metric&lang=zh_cn&callback=?';
-                var data_forecast = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&cnt=' + cnt + '&units=metric&lang=zh_cn&callback=?';
-                var data_temperature = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&cnt=15&units=metric&lang=zh_cn&callback=?';
+                var data_current = 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&lang=zh_cn&APPID=607cd461b96f865d29044d4deb1bddf4&callback=?';
+                var data_hourly = 'http://api.openweathermap.org/data/2.5/forecast/hourly?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&cnt=1&units=metric&lang=zh_cn&APPID=607cd461b96f865d29044d4deb1bddf4&callback=?';
+                var data_forecast = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&cnt=' + cnt + '&units=metric&lang=zh_cn&APPID=607cd461b96f865d29044d4deb1bddf4&callback=?';
+                var data_temperature = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&cnt=15&units=metric&lang=zh_cn&APPID=607cd461b96f865d29044d4deb1bddf4&callback=?';
                 juicer.register('getDates', getDates);
                 juicer.register('getTimes', getTimes);
                 juicer.register('degToCompass', degToCompass);
